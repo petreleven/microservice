d83ad854e3c64d4ed5f0dc03c7291e47c53dfcde
Peter Ndungu <93506710+petreleven@users.noreply.github.com>
2023-11-01 18:33:23 +0300

    Delete flask2 directory

--- flask2/htmx_reloader.py
+++
@@ -1,7 +0,0 @@
-from flask import render_template
-
-
-def htmx_template_selector(request, partial_html, full_html, context=None):
-    if request.headers.get('Hx-Request') != None:
-        return render_template(partial_html, **context)
-    return render_template(full_html, **context)
